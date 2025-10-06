352ad6127147dcd2d34bb91481c0934212c151a0
roflsunriz <roflsunriz@yahoo.co.jp>
2025-10-06 14:41:59 +0900

    chore: CHANGELOG.mdにv1.3.0の修正内容を追加し、twitter-thread-copierのバージョンを5.3.0に更新しました。また、ツイート翻訳機能の改善とUIのドラッグ＆ドロップ機能を追加しました。

--- dist/twitter-thread-copier.meta.js
+++ dist/twitter-thread-copier.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         twitter-thread-copier
 // @namespace    twitterThreadCopier
-// @version      5.2.0
+// @version      5.3.0
 // @author       roflsunriz
 // @description  Copy entire Twitter/X threads with formatting and expansions.
 // @license      MIT
