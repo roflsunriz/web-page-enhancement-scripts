16df0727e33ecefeef7298522f6bf8e35c06d3aa
roflsunriz <roflsunriz@yahoo.co.jp>
2025-10-14 15:39:44 +0900

    chore: バージョンを1.7.3に更新し、CHANGELOG.mdにv1.7.3の変更内容を追加しました。twitter-thread-copierのバージョンを6.1.0に更新し、ローカルAIでの翻訳時に単位換算を行う機能を追加しました。

--- dist/twitter-thread-copier.meta.js
+++ dist/twitter-thread-copier.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         twitter-thread-copier
 // @namespace    twitterThreadCopier
-// @version      6.0.0
+// @version      6.1.0
 // @author       roflsunriz
 // @description  Copy entire Twitter/X threads with formatting and expansions.
 // @license      MIT
