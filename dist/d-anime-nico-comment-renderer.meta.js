d5b13712209ff5616f8691e2b115da8a24bf9bfd
roflsunriz <roflsunriz@yahoo.co.jp>
2025-10-31 19:59:00 +0900

    style(d-anime): 設定保存ボタンと検索ボタンのホバーアニメーション

--- dist/d-anime-nico-comment-renderer.meta.js
+++ dist/d-anime-nico-comment-renderer.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         d-anime-nico-comment-renderer
 // @namespace    dAnimeNicoCommentRenderer
-// @version      6.2.2
+// @version      6.2.3
 // @author       roflsunriz
 // @description  Render NicoNico style comments on dAnime Store player.
 // @license      MIT
