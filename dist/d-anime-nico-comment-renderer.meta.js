2cc4ca793e3862cb11866c50ed7e3bca12bd1109
roflsunriz <roflsunriz@yahoo.co.jp>
2025-11-02 02:13:13 +0900

    chore: comment-overlay を v1.0.1 に更新

--- dist/d-anime-nico-comment-renderer.meta.js
+++ dist/d-anime-nico-comment-renderer.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         d-anime-nico-comment-renderer
 // @namespace    dAnimeNicoCommentRenderer
-// @version      6.5.0
+// @version      6.5.1
 // @author       roflsunriz
 // @description  Render NicoNico style comments on dAnime Store player.
 // @license      MIT
