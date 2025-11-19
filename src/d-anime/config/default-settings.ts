cff5f1d171b4e561c1548a83540905f076e8bc45
roflsunriz <roflsunriz@yahoo.co.jp>
2025-11-19 23:17:18 +0900

    chore: update dependencies and increment d-anime-nico-comment-renderer version to 6.14.0, add shadow intensity setting

--- src/d-anime/config/default-settings.ts
+++ src/d-anime/config/default-settings.ts
@@ -6,9 +6,10 @@ export { DEFAULT_RENDERER_SETTINGS } from "comment-overlay";
 export const cloneDefaultSettings = (): RendererSettings => ({
   ...cloneOverlayDefaults(),
   enableAutoHardReset: true,
+  shadowIntensity: "strong"
 });
 
 
 // RENDERER_VERSIONはcomment-overlayのバージョンではなく、d-anime-nico-comment-rendererユーザースクリプトのバージョンです。
 // なので変更しないこと！！！ UIにd-anime-nico-comment-rendererのバージョンが表示されるためのものです。
-export const RENDERER_VERSION = "v6.13.2";
+export const RENDERER_VERSION = "v6.14.0";
