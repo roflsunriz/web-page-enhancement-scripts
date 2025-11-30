73d0d3becf979b852addd6f973b16d31199eb870
roflsunriz <roflsunriz@yahoo.co.jp>
2025-12-01 01:17:57 +0900

    feat(d-anime): update comment-overlay to v2.9.0 and increment user script version to v6.15.14, removing hardReset method and enableAutoHardReset settings

--- src/d-anime/config/default-settings.ts
+++ src/d-anime/config/default-settings.ts
@@ -5,11 +5,10 @@ export { DEFAULT_RENDERER_SETTINGS } from "comment-overlay";
 
 export const cloneDefaultSettings = (): RendererSettings => ({
   ...cloneOverlayDefaults(),
-  enableAutoHardReset: true,
   shadowIntensity: "strong"
 });
 
 
 // USERSCRIPT_VERSION_UI_DISPLAYはcomment-overlayのバージョンではなく、d-anime-nico-comment-rendererユーザースクリプトのバージョンです。
 // なので変更しないこと！！！ UIにd-anime-nico-comment-rendererのバージョンが表示されるためのものです。
-export const USERSCRIPT_VERSION_UI_DISPLAY = "v6.15.13";
+export const USERSCRIPT_VERSION_UI_DISPLAY = "v6.15.14";
