1eaa38d3cc8fd04be75f5926286d247ecdc9ac7c
roflsunriz <roflsunriz@yahoo.co.jp>
2026-05-09 12:14:29 +0900

    chore: update user script version to v7.3.11 and adjust tsconfig paths

--- dist/d-anime-nico-comment-renderer.meta.js
+++ dist/d-anime-nico-comment-renderer.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         d-anime-nico-comment-renderer
 // @namespace    dAnimeNicoCommentRenderer
-// @version      7.3.10
+// @version      7.3.11
 // @author       roflsunriz
 // @description  Render NicoNico style comments on dAnime Store player.
 // @license      MIT
--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ const danimeCfRankingMeta: MonkeyUserScript = {
 const danimeMeta: MonkeyUserScript = {
   name: 'd-anime-nico-comment-renderer',
   namespace: 'dAnimeNicoCommentRenderer',
-  version: '7.3.10',
+  version: '7.3.11',
   description: 'Render NicoNico style comments on dAnime Store player.',
   author: 'roflsunriz',
   match: [
